78e3c86bb5e5fdd24170f87d784943c459b81037
CloudDevOpsHub <100401787+CloudDevOpsHub@users.noreply.github.com>
2026-03-12 07:22:32 +0530

    Create main.tf

---
+++ checkov/main.tf
@@ -0,0 +1,10 @@
+resource "aws_security_group" "example" {
+  name = "allow-all"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
